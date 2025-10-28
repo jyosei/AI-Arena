461a6cf3f8149781151539afaa56283bc550f317
4tissimo <17526531257@163.com>
2025-10-28 21:53:20 +0800

    1.0基础框架搭建完成

--- frontend/src/api/request.js
+++ frontend/src/api/request.js
@@ -1,3 +1,8 @@
+// 文件：request.js (请确认已修改)
+
 import axios from 'axios';
-const request = axios.create({ baseURL: 'http://localhost:8000/api/' });
+
+// 必须将 localhost 更改为 Docker Compose 服务名称 'backend'
+const request = axios.create({ baseURL: 'http://backend:8000/api/' }); 
+
 export default request;
